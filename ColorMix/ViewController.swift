227e69a62b7da820e9dca9c33fefa24958d0d82e
Artem Pavlov <pavlov.tema@gmail.com>
2021-08-20 22:54:39 +0500

    Cod is work, colours are mixing with sliders. Maybe cod will be better in future.

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -9,11 +9,77 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorPlace: UIView!
+    
+    @IBOutlet var redColor: UILabel!
+    @IBOutlet var greenColor: UILabel!
+    @IBOutlet var blueColor: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorPlace.layer.cornerRadius = 15
+    
+        redSlider.value = 1
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+       
+        greenSlider.value = 1
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        
+        blueSlider.value = 1
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        
+        redColor.text = String(format: "%.2f", redSlider.value)
+        greenColor.text = String(format: "%.2f", greenSlider.value)
+        blueColor.text = String(format: "%.2f", blueSlider.value)
     }
-
-
+    
+    @IBAction func redSliderAction() {
+        
+        let redValue = CGFloat(redSlider.value)
+        let greenValue = CGFloat(greenSlider.value)
+        let blueValue = CGFloat(blueSlider.value)
+        
+        redColor.text = String(format: "%.2f", redSlider.value)
+        
+        
+        colorPlace.backgroundColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: 1)
+    }
+    
+    @IBAction func greenSliderAction() {
+        
+        let redValue = CGFloat(redSlider.value)
+        let greenValue = CGFloat(greenSlider.value)
+        let blueValue = CGFloat(blueSlider.value)
+        
+        greenColor.text = String(format: "%.2f", greenSlider.value)
+        
+        colorPlace.backgroundColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: 1)
+    }
+    
+    @IBAction func blueSliderAction() {
+        
+        let redValue = CGFloat(redSlider.value)
+        let greenValue = CGFloat(greenSlider.value)
+        let blueValue = CGFloat(blueSlider.value)
+        
+        blueColor.text = String(format: "%.2f", blueSlider.value)
+        
+        colorPlace.backgroundColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: 1)
+    }
+    
 }
+   
+        
+    
+    
+
+
 
